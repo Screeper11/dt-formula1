57137af4c0df1ea0428b9bd751d1dc34254f596f
Papp Bence <bencep4pp@gmail.com>
2024-01-26 20:25:36 +0100

    CORS fix

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,2 +1,2 @@
-// export const backendEndpoint: string = "https://backend.formula.screeper.dev"
-export const backendEndpoint: string = "https://backend.railway.internal"
+export const backendEndpoint: string = "https://backend.formula.screeper.dev"
+// export const backendEndpoint: string = "https://backend.railway.internal"
