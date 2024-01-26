c6f51abd352985cd9eb85bbd69927c25680a8629
Papp Bence <bencep4pp@gmail.com>
2024-01-26 20:29:40 +0100

    Cleanup

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,2 +1,1 @@
-export const backendEndpoint: string = "https://backend.formula.screeper.dev"
-// export const backendEndpoint: string = "https://backend.railway.internal"
+export const backendEndpoint: string = "https://backend.formula.screeper.dev";
