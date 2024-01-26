9e7650c9e68ed81a368daa0e948dcc1cc81a87bd
Papp Bence <bencep4pp@gmail.com>
2024-01-26 15:20:57 +0100

    Backend - First draft

---
+++ backend/src/DriversManager.ts
@@ -0,0 +1,51 @@
+export class DriversManager {
+  drivers: Driver[];
+
+  constructor(drivers: Driver[]) {
+    this.drivers = drivers;
+    this.randomizeDriversOrder();
+    this.updateDriversData();
+  }
+
+  randomizeDriversOrder() {
+    this.drivers.sort(() => Math.random() - 0.5);
+  }
+
+  updateDriversData() {
+    this.drivers.forEach((driver, index) => {
+      driver.imgUrl = `/static/${driver.code.toLowerCase()}.png`;
+      driver.place = index + 1;
+    });
+  }
+
+  findDriverById(id: number): Driver | undefined {
+    return this.drivers.find(driver => driver.id === id);
+  }
+
+  canOvertake(driver: Driver | undefined): boolean {
+    return driver !== undefined && driver.place > 1;
+  }
+
+  performOvertake(driver: Driver) {
+    const overtakenDriver = this.drivers.find(d => d.place === driver.place - 1);
+    if (overtakenDriver) {
+      driver.place--;
+      overtakenDriver.place++;
+    }
+  }
+
+  compareByPlace(a: Driver, b: Driver): number {
+    return a.place - b.place;
+  }
+}
+
+interface Driver {
+  id: number;
+  code: string;
+  firstname: string;
+  lastname: string;
+  country: string;
+  team: string;
+  imgUrl: string;
+  place: number;
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,42 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import {DriversManager} from "./DriversManager";
 
 const app = express();
 const port = 3001;
 
+// Load data
 app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, '/data/img')));
+const driversManager: DriversManager = (() => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, '/data/drivers.json'));
+    const drivers = JSON.parse(data.toString());
+    return new DriversManager(drivers);
+  } catch (err) {
+    throw err;
+  }
+})();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.get('/api/drivers', (req, res) => {
+  res.json(driversManager);
 });
+
+app.post('/api/drivers/:driverId/overtake', (req, res) => {
+  const driverId = parseInt(req.params.driverId);
+  const driver = driversManager.findDriverById(driverId);
+
+  if (driver === undefined || !driversManager.canOvertake(driver)) {
+    res.status(400).send();
+    return;
+  }
+
+  driversManager.performOvertake(driver);
+  res.status(200).send();
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
