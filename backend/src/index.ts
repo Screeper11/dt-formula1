7ca6a7ca6198cf3bf4412c6a92971087d42c2c64
Papp Bence <bencep4pp@gmail.com>
2024-01-26 20:06:09 +0100

    CORS fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import {DriversManager} from "./DriversManager";
 
 // Config server
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.use(cors({
   origin: 'https://formula.screeper.dev',
   credentials: true
